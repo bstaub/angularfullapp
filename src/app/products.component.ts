a67b65b079a02d67e399b34b47e5eba4aa5db8e5
Bruno Staub <bruno.staub@gmail.com>
2018-07-10 13:35:32 +0200

    *ngIf and *ngFor Directives

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -9,10 +9,16 @@ import {Component} from '@angular/core';
 export class ProductsComponent {
   productName = 'a book';
   isDisabled = true;
+  products = ['A Book', 'A Tree'];
   constructor() {
      setTimeout(() => {
        // this.productName = 'after 3 seconds a tree appears!';
        this.isDisabled = false;
      }, 3000);
   }
+
+  onAddProduct() {
+    this.products.push(this.productName);
+  }
+
 }
